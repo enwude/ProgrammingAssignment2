d10b39f588bbf68ed0dccccf599f9e21abb4cd4b
enwude <enwude@aol.com>
2015-06-18 22:00:14 -0400

    Programming Assignment 2
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,22 @@
 makeCacheMatrix <- function(x = matrix()) {
         matrixInv <- NULL
         
+        # setMat recieves an argument and stores it as a matrix in variable x
+        # Function setMat also resets the matrixInv cache to NULL for a new computation
         setMat <- function(y){
                 x <<- y
                 matrixInv <<- NULL
         }
         
+        # getMat returns the stored matrix
         getMat <- function(){x}
         
+        # setInv caches the matrix inverse in matrixInv
         setInv <- function(inverse){
                 matrixInv <<- inverse
         }
         
+        # function getInv retrieves the cached inverse from matrixInv
         getInv <- function(){matrixInv}
         
         list(setMat = setMat, getMat = getMat, setInv = setInv, getInv = getInv)
@@ -27,17 +32,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve retrieves the inverse from the cache if it has already been computed and stored.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         
+        ## This function returns a matrix that is the inverse of 'x'
+        
+        ## matInv calls function getInv in makeCacheMatrix to return the value contained in the cache
         matInv <- x$getInv()
          if(!is.null(matInv)){
-                 message("Retrieving cached matrix inverse")
+                 message("Retrieving cached matrix inverse") ## display message if a value is contained in cache
                  return(matInv)
          }
         
-        data <- x$getMat()
-        inverse <- solve(data)
-        x$setInv(inverse)
+        data <- x$getMat() ## Call function getMat and store matrix in data
+        inverse <- solve(data) ## call function solve  to compute the inverse of matrix
+        x$setInv(inverse) ## call setInv to cache the computed inverse
         return(inverse)
                  
 }
